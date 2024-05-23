949f2676e716d950136d73352b76fb436589a093
javierNetsolin <javier.prado@netsolin.com>
2024-05-23 07:42:46 -0500

    react components mobile and destop style

--- src/Footer.js
+++ src/Footer.js
@@ -1,37 +1,41 @@
-import Logo from "./assets/Logo.svg";
+import VerticalLogo from "./assets/VerticalLogo";
 export default function Footer(){
-    return(
-    <footer>
-        <figure>
-         <img src={Logo} alt="Little Lemon Logo"/>
-        </figure>
-        <figure>
-            <figcaption>Doormat Navigation</figcaption>
-            <ul>
-                <li><a href="/home">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/menu">Menu</a></li>
-                <li><a href="/reservations">Reservations</a></li>
-                <li><a href="/order-online">Order Online</a></li>
-                <li><a href="/login">Login</a></li>
-            </ul>
-        </figure>
-        <figure>
-            <figcaption>Contact</figcaption>
-            <ul>
-                <li>Adress</li>
-                <li>Phone number</li>
-                <li>Email</li>
-            </ul>
-        </figure>
-        <figure>
-            <figcaption>Doormat Navigation</figcaption>
-            <ul>
-                <li><a href="https://www.facebook.com/">Facebook</a></li>
-                <li><a href="https://www.instagram.com">Instagram</a></li>
-                <li><a href="https://twitter.com/">Twitter</a></li>
-            </ul>
-        </figure>
-    </footer>
+    return( <footer class="main-footer">
+        <section class="footer-section">
+
+          {/* <div class="image-container"> */}
+           {/* <img src={VerticalLogo} alt="Little Lemon Logo"/> */}
+          {/* </div> */}
+            <VerticalLogo className="logo-container" ></VerticalLogo>
+
+          <figure>
+              <figcaption>Doormat Navigation</figcaption>
+              <ul>
+                  <li><a href="/home">Home</a></li>
+                  <li><a href="/about">About</a></li>
+                  <li><a href="/menu">Menu</a></li>
+                  <li><a href="/reservations">Reservations</a></li>
+                  <li><a href="/order-online">Order Online</a></li>
+                  <li><a href="/login">Login</a></li>
+              </ul>
+          </figure>
+          <figure>
+              <figcaption>Contact</figcaption>
+              <ul>
+                  <li>Adress</li>
+                  <li>Phone number</li>
+                  <li>Email</li>
+              </ul>
+          </figure>
+          <figure>
+              <figcaption>Social Media Links</figcaption>
+              <ul>
+                  <li><a href="https://www.facebook.com/">Facebook</a></li>
+                  <li><a href="https://www.instagram.com">Instagram</a></li>
+                  <li><a href="https://twitter.com/">Twitter</a></li>
+              </ul>
+          </figure>
+        </section>
+  </footer>
     )
 }
--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,14 @@
 import ResturantFood from "./assets/restauranfood.jpg"
+import GreekSalad from "./assets/greeksalad.jpg"
+import LemonDessert from "./assets/lemondessert.jpg"
+import Bruchetta from "./assets/bruchetta.jpg"
+import Moto from "./assets/moto.svg"
+import MarioAdrian from "./assets/marioandAdrian.jpg"
 export default function Main(){
     return(<main className="main-content">
         <Hero />
         <Highlights/>
+        <About />
     </main>)
 }
 
@@ -41,45 +47,118 @@ export function Highlights(){
           </div>
         </div>
         <div class="cards-h">
-          <div class="specials-card">
-           <div class="image-card-container">
-            <img  src="assets/greek salad.jpg" alt="greek salad"/>
-          </div>
-          <div class="content-container-card">
-            <div class="tittle-container-card">
-              <h4>Greek sald</h4>
-              <p>$12.99</p>
-            </div>
-            <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
-            <h5>Order  delivery <span class="icon"><img src="assets/moto.svg" alt="Shopping Cart Icon"/></span></h5>
-          </div>
-        </div>
-          <div class="specials-card">
-           <div class="image-card-container">
-            <img  src="assets/bruchetta.svg" alt="greek salad"/>
-          </div>
-          <div class="content-container-card">
-            <div class="tittle-container-card">
-              <h4>Bruchetta</h4>
-              <p>$5.99</p>
-            </div>
-            <p>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. </p>
-            <h5>Order  delivery <span class="icon"><img src="assets/moto.svg" alt="Shopping Cart Icon"/></span></h5>
-          </div>
-        </div>
-          <div class="specials-card">
-           <div class="image-card-container">
-            <img  src="assets/lemon dessert.jpg" alt="greek salad"/>
-          </div>
-          <div class="content-container-card">
-            <div class="tittle-container-card">
-              <h4>Lemon Dessert</h4>
-              <p>$5.00</p>
-            </div>
-            <p>This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined. </p>
-            <h5>Order  delivery <span class="icon"><img src="assets/moto.svg" alt="Shopping Cart Icon"/></span></h5>
-          </div>
-        </div>
+          <Cards />
+
         </div>
       </section>)
 }
+
+export function About(){
+    return( <section class="about">
+        <div class="left">
+          <div>
+            <h1>Little Lemon</h1>
+            <h3>Chicago</h3>
+
+          </div>
+          <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+  Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. </p>
+        </div>
+      <div class="right">
+
+        <div class="image-container">
+                 <img  src={MarioAdrian} alt="Mari and Adrian"/>
+               </div>
+      </div>
+      </section>)
+}
+
+export function Cards(){
+  const list = [
+    {
+      title: 'Greek sald',
+      img: GreekSalad,
+      price: '$12.99',
+      description: 'The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.',
+    },
+    {
+      title: 'Bruchetta',
+      img: Bruchetta,
+      price: '$5.99',
+      description: 'Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.',
+    },
+    {
+      title: 'Lemon Dessert',
+      img: LemonDessert,
+      price: '$5.00',
+      description: 'This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.',
+    },
+  ];
+
+  return(<>
+      {list.map((item, index)=>(
+    <div class="specials-card" key={index}>
+        <div class="image-card-container">
+          <img src={item.img} alt="greek salad" />
+        </div><div class="content-container-card">
+            <div class="tittle-container-card">
+              <h4>{item.title}</h4>
+              <p>{item.price}</p>
+            </div>
+            <p>{item.description}</p>
+            <h5>Order  delivery <span class="icon"><img src={Moto} alt="Shopping Cart Icon" /></span></h5>
+          </div>
+        </div>
+      ))}
+  </>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+//  <div class="specials-card">
+//            <div class="image-card-container">
+//             <img  src={GreekSalad} alt="greek salad"/>
+//           </div>
+//           <div class="content-container-card">
+//             <div class="tittle-container-card">
+//               <h4>Greek sald</h4>
+//               <p>$12.99</p>
+//             </div>
+//             <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
+//             <h5>Order  delivery <span class="icon"><img src={Moto} alt="Shopping Cart Icon"/></span></h5>
+//           </div>
+//         </div>
+//           <div class="specials-card">
+//            <div class="image-card-container">
+//             <img  src={Bruchetta} alt="Bruchetta"/>
+//           </div>
+//           <div class="content-container-card">
+//             <div class="tittle-container-card">
+//               <h4>Bruchetta</h4>
+//               <p>$5.99</p>
+//             </div>
+//             <p>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. </p>
+//             <h5>Order  delivery <span class="icon"><img src={Moto} alt="Shopping Cart Icon"/></span></h5>
+//           </div>
+//         </div>
+//           <div class="specials-card">
+//            <div class="image-card-container">
+//             <img  src={LemonDessert} alt="Lemon dessert"/>
+//           </div>
+//           <div class="content-container-card">
+//             <div class="tittle-container-card">
+//               <h4>Lemon Dessert</h4>
+//               <p>$5.00</p>
+//             </div>
+//             <p>This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined. </p>
+//             <h5>Order  delivery <span class="icon"><img src={Moto} alt="Shopping Cart Icon"/></span></h5>
+//           </div>
+//         </div>
